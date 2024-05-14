7959871203646976fee5df77cdfe6eaca5862927
TheBous <thebous1993@gmail.com>
2024-05-14 12:28:09 +0200

    chore(update): upgrade packages and update repo

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,11 @@
 import { sveltekit } from '@sveltejs/kit/vite';
-import { nodeLoaderPlugin } from "@vavite/node-loader/plugin";
 import { defineConfig } from 'vite';
 
 export default defineConfig({
-	plugins: [nodeLoaderPlugin(), sveltekit()]
+  plugins: [
+    sveltekit()
+  ],
+  optimizeDeps: {
+    exclude: ['farmhash']
+  }
 });
